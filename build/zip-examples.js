1f4881475a6afb1045430c4311d9956072193ec9
XhmikosR <xhmikosr@gmail.com>
2020-12-14 15:21:30 +0200

    Update zip-examples.js

--- build/zip-examples.js
+++ build/zip-examples.js
@@ -27,39 +27,42 @@ sh.cd(path.join(__dirname, '..'))
 
 // remove any previously created folder with the same name
 sh.rm('-rf', folderName)
+
 // create any folders so that `cp` works
 sh.mkdir('-p', folderName)
 sh.mkdir('-p', `${folderName}/assets/brand/`)
-
-sh.cp('-Rf', `_gh_pages/docs/${versionShort}/examples/*`, folderName)
-
 sh.mkdir('-p', `${folderName}/assets/dist/css/`)
 sh.mkdir('-p', `${folderName}/assets/dist/js/`)
 
+sh.cp('-Rf', `_gh_pages/docs/${versionShort}/examples/*`, folderName)
+
+// these are the files we need in the examples
 const cssFiles = [
   'bootstrap.min.css',
   'bootstrap.min.css.map',
-  'bootstrap.rtl.min.css.map',
-  'bootstrap.rtl.min.css'
+  'bootstrap.rtl.min.css',
+  'bootstrap.rtl.min.css.map'
 ]
 const jsFiles = [
   'bootstrap.bundle.min.js',
   'bootstrap.bundle.min.js.map'
 ]
+const imgFiles = [
+  'bootstrap-logo.svg',
+  'bootstrap-logo-white.svg'
+]
 
 cssFiles.forEach(file => {
-  sh.cp('-Rf', `_gh_pages/docs/${versionShort}/dist/css/${file}`, `${folderName}/assets/dist/css/`)
+  sh.cp('-f', `_gh_pages/docs/${versionShort}/dist/css/${file}`, `${folderName}/assets/dist/css/`)
 })
 
 jsFiles.forEach(file => {
-  sh.cp('-Rf', `_gh_pages/docs/${versionShort}/dist/js/${file}`, `${folderName}/assets/dist/js/`)
+  sh.cp('-f', `_gh_pages/docs/${versionShort}/dist/js/${file}`, `${folderName}/assets/dist/js/`)
 })
 
-// also copy the two brand images we use in the examples
-sh.cp('-f', [
-  `_gh_pages/docs/${versionShort}/assets/brand/bootstrap-logo.svg`,
-  `_gh_pages/docs/${versionShort}/assets/brand/bootstrap-logo-white.svg`
-], `${folderName}/assets/brand/`)
+imgFiles.forEach(file => {
+  sh.cp('-f', `_gh_pages/docs/${versionShort}/assets/brand/${file}`, `${folderName}/assets/brand/`)
+})
 
 sh.rm(`${folderName}/index.html`)
 
